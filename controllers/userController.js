16e1535aed2064809817a50a53758f7cd3c9527f
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-09-30 17:41:40 -0300

    register user in mongodb

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,28 @@
-export const register = (req, res, nex) => {
-  console.log(req.body);
+import { User } from "../model/userModels.js";
+import bcrypt from "bcrypt";
+
+export const register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const userNameCheck = await User.findOne({ username });
+    if (userNameCheck)
+      return res.json({ msg: "This username is already used", status: false });
+
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck)
+      return res.json({ msg: "This email is already used", status: false });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    delete user.password;
+
+    return res.json({ status: true, user });
+  } catch (err) {
+    next(err);
+  }
 };
