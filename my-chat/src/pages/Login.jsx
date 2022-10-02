e5855143fc6e42bc9b594ac6dba455d1af89cac7
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-02 18:02:26 -0300

    login

--- my-chat/src/pages/Login.jsx
+++ my-chat/src/pages/Login.jsx
@@ -1,5 +1,155 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { toastOptions } from "../utils/toastify";
+import axios from "axios";
+import { loginRoute } from "../utils/apiRoutes";
 
 export default function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (handleValidation()) {
+      const { password, username, email } = values;
+      const { data } = await axios.post(loginRoute, {
+        username,
+        email,
+        password,
+      });
+      if (!data.status) {
+        toast.error(data.msg, toastOptions);
+      } else {
+        localStorage.setItem("chatty-user", JSON.stringify(data.user));
+        navigate("/");
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleValidation = () => {
+    const { password, username } = values;
+    if (password === "") {
+      toast.error("Email and Password are required", {
+        toastOptions,
+      });
+      return false;
+    } else if (username.length === "") {
+      toast.error("Email and Password are required", {
+        toastOptions,
+      });
+      return false;
+    }
+    return true;
+  };
+
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="brand">
+            <img src="logo.svg" alt="" />
+            <h1>snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+            min="3"
+          ></input>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          ></input>
+          <button type="submit">Login</button>
+          <span>
+            Don't have an account yet? <Link to="/register">Register</Link>
+          </span>
+        </form>
+        <ToastContainer />
+      </FormContainer>
+    </>
+  );
 }
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      color: white;
+      font-size: 1rem;
+      &:focus {
+        border: 0.1rem solid #997ad0;
+        outline: none;
+      }
+    }
+    button {
+      background-color: #997af0;
+      color: white;
+      padding: 1rem 2rem;
+      border: none;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      transition: 0.5s ease-in-out;
+      &:hover {
+        background-color: #4e0eff;
+      }
+    }
+    span {
+      color: white;
+      text-transform: uppercase;
+      a {
+        color: #4e0eff;
+        font-weight: bold;
+        text-decoration: none;
+      }
+    }
+  }
+`;
