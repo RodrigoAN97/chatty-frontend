699303f37fcb30cdb0b8b93ef2b06157fa2a8b5e
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-03 11:10:44 -0300

    set avatar and loader

--- my-chat/src/App.js
+++ my-chat/src/App.js
@@ -53,6 +53,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
+import SetAvatar from "./pages/SetAvatar";
 
 export default function App() {
   return (
@@ -61,6 +62,7 @@ export default function App() {
         <Route path="/" element={<Chat />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/set-avatar" element={<SetAvatar />} />
       </Routes>
     </BrowserRouter>
   );
