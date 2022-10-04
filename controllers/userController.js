963088a6a22024c48e203fd5b66e5bd700fe4ea0
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-04 11:18:46 -0300

    get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,3 +68,18 @@ export const setAvatar = async (req, res, next) => {
     next(err);
   }
 };
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    console.log(users);
+    return res.json(users);
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { login, register, setAvatar } from "../controllers/userController.js";
+import {
+  login,
+  register,
+  setAvatar,
+  getAllUsers,
+} from "../controllers/userController.js";
 
 export const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
 userRouter.post("/set-avatar/:id", setAvatar);
+userRouter.get("/all-users/:id", getAllUsers);
