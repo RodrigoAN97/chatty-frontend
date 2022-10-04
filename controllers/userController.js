004fa2580d4b21ee3bbc4912761d7e0bb4bb21bc
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-04 09:41:31 -0300

    user variable to return updated version

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,10 +52,14 @@ export const setAvatar = async (req, res, next) => {
   try {
     const userId = req.params.id;
     const avatarImage = req.body.image;
-    const userData = await User.findByIdAndUpdate(userId, {
-      isAvatarImageSet: true,
-      avatarImage,
-    });
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      {
+        isAvatarImageSet: true,
+        avatarImage,
+      },
+      { new: true }
+    );
     return res.json({
       isSet: userData.isAvatarImageSet,
       image: userData.avatarImage,
