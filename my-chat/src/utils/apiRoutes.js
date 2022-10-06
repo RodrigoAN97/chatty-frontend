31f299cb1f31bd445d0f6a0d9d287e2565324b86
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-06 10:00:03 -0300

    add message

--- my-chat/src/utils/apiRoutes.js
+++ my-chat/src/utils/apiRoutes.js
@@ -5,3 +5,4 @@ export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/set-avatar`;
 export const allUsersRoute = `${host}/api/auth/all-users`;
 export const getAvatarsRoute = `${host}/api/auth/get-avatars`;
+export const sendMessageRoute = `${host}/api/messages/add-message`;
