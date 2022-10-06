1e8fee28e457382d999489ae12c49ffb42a8cc52
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-06 10:00:03 -0300

    add message

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,2 +1,21 @@
-export const addMessage = async (req, res, next) => {};
+import { Messages } from "../model/messageModel";
+
+export const addMessage = async (req, res, next) => {
+  try {
+    const { from, to, message } = req.body;
+    const data = await Messages.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+    if (data) {
+      return res.json({ msg: "Message added successfully" });
+    } else {
+      res.json({ msg: "Failed to add message to the database" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
 export const getAllMessages = async (req, res, next) => {};
