ae36ce615ec88bf9a880dbc229662212c044d309
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-13 10:20:57 -0300

    get avatars route does not exist anymore

--- my-chat/src/utils/apiRoutes.js
+++ my-chat/src/utils/apiRoutes.js
@@ -4,7 +4,6 @@ export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/set-avatar`;
 export const allUsersRoute = `${host}/api/auth/all-users`;
-export const getAvatarsRoute = `${host}/api/auth/get-avatars`;
 
 export const sendMessageRoute = `${host}/api/messages/add-message`;
 export const getAllMessagesRoute = `${host}/api/messages/get-messages`;
